d5f53cdc29089f302de09f4e73628eb795175d65
quantumbuilder32 <quantumbuilder32@gmail.com>
2024-07-26 00:37:41 -0500

    cool

--- serverFunctions/handleGPT.tsx
+++ serverFunctions/handleGPT.tsx
@@ -4,9 +4,9 @@ import { keyword } from "@/types";
 
 const apiKey = process.env.OPENAI_API_KEY || ""
 
-// const openai = new OpenAI({
-//     apiKey: apiKey
-// })
+const openai = new OpenAI({
+    apiKey: apiKey
+})
 
 const getTopTrendsPrompt =
     `
@@ -20,32 +20,31 @@ really important the data I receive is like the example. Don't give any other co
 `
 
 export async function getTopKeywords(): Promise<keyword[]> {
-    return [{ name: "" }]
-    // const response = await openai.chat.completions.create({
-    //     model: "gpt-4o",
-    //     messages: [
-    //         {
-    //             role: "user",
-    //             content: getTopTrendsPrompt
-    //         }
-    //     ],
-    //     temperature: .7,
-    //     max_tokens: 2000
-    // })
+    const response = await openai.chat.completions.create({
+        model: "gpt-4o",
+        messages: [
+            {
+                role: "user",
+                content: getTopTrendsPrompt
+            }
+        ],
+        temperature: .7,
+        max_tokens: 2000
+    })
 
-    // let textResponse = response.choices[0].message.content
-    // if (textResponse === null) throw new Error("no text response from gpt")
+    let textResponse = response.choices[0].message.content
+    if (textResponse === null) throw new Error("no text response from gpt")
 
-    // if (textResponse.includes("sorry")) throw new Error("gpt didnt give proper response")
+    if (textResponse.includes("sorry")) throw new Error("gpt didnt give proper response")
 
-    // const keywords: keyword[] = textResponse.split(",").map(eachString => {
-    //     const keyWord: keyword = {
-    //         name: eachString
-    //     }
-    //     return keyWord
-    // })
+    const keywords: keyword[] = textResponse.split(",").map(eachString => {
+        const keyWord: keyword = {
+            name: eachString
+        }
+        return keyWord
+    })
 
-    // return keywords
+    return keywords
 }
 
 export async function getAPIKey() {
@@ -53,21 +52,20 @@ export async function getAPIKey() {
 }
 
 export async function getGptVideoScript(prompt: string): Promise<string | undefined> {
-    return ""
-    // const response = await openai.chat.completions.create({
-    //     model: "gpt-4o",
-    //     messages: [
-    //         {
-    //             role: "user",
-    //             content: prompt
-    //         }
-    //     ],
-    //     temperature: 1,
-    //     //1750 tokens, 7000 characters per average script - 1 token = 4 characters
-    // })
+    const response = await openai.chat.completions.create({
+        model: "gpt-4o",
+        messages: [
+            {
+                role: "user",
+                content: prompt
+            }
+        ],
+        temperature: 1,
+        //1750 tokens, 7000 characters per average script - 1 token = 4 characters
+    })
 
-    // const textResponse = response.choices[0].message.content
-    // if (textResponse === null) return
+    const textResponse = response.choices[0].message.content
+    if (textResponse === null) return
 
-    // return textResponse
+    return textResponse
 }
